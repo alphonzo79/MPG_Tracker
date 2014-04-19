2e388e444bb81a9ced7b067b24d150497cdc182d
Joe Rowley Laptop <joe.rowley@bodybuilding.com>
2014-04-18 21:15:57 -0600

    Setting up layout for dashboard activity

--- MpgTracker_Android/src/main/java/rowley/mpgtracker/DashboardActivity.java
+++ MpgTracker_Android/src/main/java/rowley/mpgtracker/DashboardActivity.java
@@ -1,15 +1,25 @@
 package rowley.mpgtracker;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
 
-public class DashboardActivity extends Activity {
+public class DashboardActivity extends Activity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
+
+        findViewById(R.id.settings_button).setOnClickListener(this);
+    }
+
+    public void onClick(View view) {
+        switch(view.getId()) {
+            case R.id.settings_button:
+                startActivity(new Intent(this, SettingsActivity.class));
+                break;
+        }
     }
 }
---
+++ MpgTracker_Android/src/main/java/rowley/mpgtracker/SettingsActivity.java
@@ -0,0 +1,7 @@
+package rowley.mpgtracker;
+
+import android.app.Activity;
+import android.app.ListActivity;
+
+public class SettingsActivity extends Activity {
+}
