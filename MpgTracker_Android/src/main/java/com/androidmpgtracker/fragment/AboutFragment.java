a63a071e642a93bc3ba0c15737863092c652d25c
Joe Rowley Laptop <joe.rowley@bodybuilding.com>
2014-04-22 22:16:45 -0600

    Formatting about text with html

--- MpgTracker_Android/src/main/java/com/androidmpgtracker/fragment/AboutFragment.java
+++ MpgTracker_Android/src/main/java/com/androidmpgtracker/fragment/AboutFragment.java
@@ -2,10 +2,11 @@ package com.androidmpgtracker.fragment;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.androidmpgtracker.R;
 
@@ -14,6 +15,10 @@ public class AboutFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_about, null);
+
+        if(root != null) {
+            ((TextView) root.findViewById(R.id.about_text)).setText(getText(R.string.about_text));
+        }
 
         return root;
     }
