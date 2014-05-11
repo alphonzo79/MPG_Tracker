db4549bfb0c4da984fe1e6ab9aa57028ac88e2a2
Joe Rowley Laptop <joe.rowley@bodybuilding.com>
2014-05-10 23:38:36 -0600

    Establishing api server module. Barebones index

---
+++ AndroidMpgTracker_ApiServer/src/com/android_mpg_tracker/index.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: joe
+ * Date: 5/10/14
+ * Time: 9:41 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+
+
+Print <<<HEADER
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+	<head>
+		<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+		<title>Android MPG Tracker</title>
+
+		<meta name="description" content="API Server for Android MPG Tracker" />
+		<meta name="copyright" content="Copyright 2014 AndroidMpgTracker.com" />
+
+    </head>
+
+    <body>
+
+        <h1>This is a holding page for the Android MPG Tracker API. It doesn't do much. It isn't even pretty. All the fun stuff happens where you can't see it</h1>
+
+    </body>
+
+</html>
+
+HEADER;
+
+?>
