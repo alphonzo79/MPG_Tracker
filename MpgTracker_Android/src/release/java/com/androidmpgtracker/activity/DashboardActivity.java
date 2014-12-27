e1d8e88c1aaca30053e9bbb8b38ac53c3a86b4c1
Joe Rowley <jrowley2@gmail.com>
2014-12-26 21:10:00 -0700

    Setting up crashlytics for paid version

--- MpgTracker_Android/src/release/java/com/androidmpgtracker/activity/DashboardActivity.java
+++ MpgTracker_Android/src/release/java/com/androidmpgtracker/activity/DashboardActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import com.androidmpgtracker.MpgApplication;
 import com.androidmpgtracker.R;
 import com.flurry.android.FlurryAgent;
+import com.crashlytics.android.Crashlytics;
 
 /**
  * Created by joe on 12/26/14.
@@ -22,7 +23,7 @@ public class DashboardActivity extends DashboardActivityRoot {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
-//        Crashlytics.start(this);
+        Crashlytics.start(this);
         super.onCreate(savedInstanceState);
     }
 }
