705b655283700f2fbefff83a69184330f5f3402d
Joe Rowley <jrowley2@gmail.com>
2014-12-26 21:54:53 -0700

    Setting up ads for free version

--- MpgTracker_Android/src/freeFlavor/java/com/androidmpgtracker/activity/ReportingActivity.java
+++ MpgTracker_Android/src/freeFlavor/java/com/androidmpgtracker/activity/ReportingActivity.java
@@ -1,7 +1,24 @@
 package com.androidmpgtracker.activity;
 
+import android.os.Bundle;
+
+import com.androidmpgtracker.R;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 /**
  * Created by joe on 12/26/14.
  */
 public class ReportingActivity extends ReportingActivityRoot {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AdView mAdView = (AdView) findViewById(R.id.adView);
+//        AdRequest adRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
+        AdRequest adRequest = new AdRequest.Builder().addTestDevice(AdRequest.DEVICE_ID_EMULATOR).build();
+        mAdView.loadAd(adRequest);
+
+    }
 }
